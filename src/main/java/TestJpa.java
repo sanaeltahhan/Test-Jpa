b8f4d49ece7310e45d9ecda904eeddf550b43bb8
sanaeltahhan <sana.eltahhan@hotmail.fr>
2020-07-02 14:52:09 +0200

    TP 2 : JPA

--- src/main/java/TestJpa.java
+++ src/main/java/TestJpa.java
@@ -1,12 +1,72 @@
+import java.util.List;
+import java.util.stream.Stream;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
+import fr.diginamic.entity.Livre;
 
 public class TestJpa {
 
 	public static void main(String[] args) {
-        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("pu_essai");
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("pu_bibli");
         EntityManager entityManager = entityManagerFactory.createEntityManager();
+        
+     
+        // Réalisez un find simple permettant d’extraire un livre en fonction de son identifiant et  affichez son titre et son auteur.
+        Livre livre = entityManager.find(Livre.class, 1);
+        System.out.println(livre);
+        
+        // Insérez un nouveau Livre de votre choix en base de données
+        
+        Livre livreInsert = new Livre();
+        livreInsert.setId(6);
+        livreInsert.setTitre("La peste");
+        livreInsert.setAuteur("Albert Camus");
+        
+     // Constrainte JPA => la modification doit se faire dans le cadre d'une transaction.
+        EntityTransaction entityTransa = entityManager.getTransaction();
+        entityTransa.begin();
+        entityManager.persist(livreInsert); // insérer une nouvelle ligne
+        entityTransa.commit(); 
+
+        // Modifiez le titre du livre d’identifiant 5 qui possède une erreur : le nouveau titre doit être
+        // « Du plaisir dans la cuisine » au lieu de « 1001 recettes de Cuisine ».
+        
+        Livre livreModif = entityManager.find(Livre.class, 5);
+        livreModif.setTitre("Du plaisir dans la cuisine");
+        
+        EntityTransaction entityTransaModif = entityManager.getTransaction();
+        entityTransaModif.begin();
+        entityManager.persist(livreModif); // insérer une nouvelle ligne modifier
+        entityTransaModif.commit(); 
+        
+        // Faites une requête JPQL pour extraire de la base un livre en fonction de son titre.
+        TypedQuery<Livre> querySelect = entityManager.createQuery("select l from Livre as l where TITRE like 'La peste' ", Livre.class);
+        System.out.println(querySelect.getSingleResult());
+        
+        // Faites une requête JPQL pour extraire de la base un livre en fonction de son auteur.
+        TypedQuery<Livre> querySelectAuteur = entityManager.createQuery("select l from Livre as l where AUTEUR like 'Albert Camus' ", Livre.class);
+        System.out.println(querySelectAuteur.getSingleResult());
+        
+        // Supprimez un livre de votre choix en base de données.
+        Livre livreSupp = entityManager.find(Livre.class, 2);
+        
+        EntityTransaction entityTransaSup = entityManager.getTransaction();
+        entityTransaSup.begin();
+        entityManager.remove(livreSupp); // Sup une ligne 
+        entityTransaSup.commit(); 
+        
+        // Affichez la liste de tous les livres présents en base de données (titre et auteur).
+        TypedQuery<Livre> querySelectAll = entityManager.createQuery("select l from Livre as l", Livre.class);
+        List<Livre> listeLivre = querySelectAll.getResultList();
+        System.out.println();
+       
+        listeLivre.forEach(System.out::println);
+        
         
         entityManager.close();
 
---
+++ src/main/java/fr/diginamic/entity/Livre.java
@@ -0,0 +1,85 @@
+package fr.diginamic.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "LIVRE")
+public class Livre {
+	
+	@Id
+    @Column(name = "ID")
+	private Integer id;
+	
+    @Column(name = "TITRE")
+	private String titre;
+
+    @Column(name = "AUTEUR")
+	private String auteur;
+
+	/** Constructeur
+	 * @param id
+	 */
+	public Livre(Integer id) {
+		this.id = id;
+	}
+	
+	/** Constructeur vide
+	 * @param id
+	 */
+	public Livre () {};
+	
+	
+
+	@Override
+	public String toString() {
+		return "Le Titre du livre est : " + titre + ", l'auteur est : "+ auteur + ", et l'Id est : " + id ;
+	}
+
+	/** Getter
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
+
+	/** Setter
+	 * @param id the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	/** Getter
+	 * @return the titre
+	 */
+	public String getTitre() {
+		return titre;
+	}
+
+	/** Setter
+	 * @param titre the titre to set
+	 */
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+
+	/** Getter
+	 * @return the auteur
+	 */
+	public String getAuteur() {
+		return auteur;
+	}
+
+	/** Setter
+	 * @param auteur the auteur to set
+	 */
+	public void setAuteur(String auteur) {
+		this.auteur = auteur;
+	}
+	
+	
+
+}
