8389b84fe9d4c9ef938ef1f51f7642a9a52fbb45
sanaeltahhan <sana.eltahhan@hotmail.fr>
2020-07-02 12:02:33 +0200

    TP 1 : test JPA

---
+++ src/main/java/TestJpa.java
@@ -0,0 +1,20 @@
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class TestJpa {
+
+	public static void main(String[] args) {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("pu_essai");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        
+        entityManager.close();
+
+
+        entityManagerFactory.close();
+
+
+
+	}
+
+}
